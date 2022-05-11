941a070a72613048da118f6e935c2ab48a417e1a
Ivan Leung <ivanl8866@gmail.com>
2022-05-10 23:37:53 -0700

    Add member functions to class Account and two new class

--- account.h
+++ account.h
@@ -1,5 +1,6 @@
 // Account class with a constructor that initializes the account name.
 #include <string>
+#include <stdexcept>
 
 class Account
 {
@@ -7,6 +8,18 @@ public:
    // constructor initializes data member name with parameter accountName
    explicit Account(std::string accountName)
        : name{accountName}
+   { // member initializer
+      // empty body
+   }
+
+   explicit Account(double accountBalance)
+       : balance{accountBalance}
+   { // member initializer
+      // empty body
+   }
+
+   explicit Account(std::string accountName, double accountBalance)
+       : name{accountName}, balance{accountBalance}
    { // member initializer
       // empty body
    }
@@ -23,6 +36,57 @@ public:
       return name;
    }
 
+   void setBalance(double accountBalance)
+   {
+      if (accountBalance < 0)
+         throw std::invalid_argument("Initial balance must be equal or greater than zero!");
+      balance = accountBalance;
+   }
+
+   double getBalance() const
+   {
+      return balance;
+   }
+
+   void credit(double deposit)
+   {
+      balance += deposit;
+   }
+
+   bool debit(double charge)
+   {
+      if (charge > balance)
+         throw std::out_of_range("Insufficient balance!");
+      balance -= charge;
+      return true;
+   }
+
 private:
    std::string name; // account name data member
+   double balance; // account balance
 };                   // end class Account
+
+class SavingsAccount : Account
+{
+public:
+   explicit SavingsAccount(std::string accountName, double accountBalance, double interestRate) : Account(accountName, accountBalance)
+   {}
+
+   double calculateInterest() const
+   {
+      return getBalance() * interestRate;
+   }
+
+private:
+   double interestRate;
+};
+
+class CheckingAccount : Account
+{
+public:
+   explicit CheckingAccount(std::string accountName, double accountBalance, double transactionFee) : Account(accountName, accountBalance)
+   {}
+
+private:
+   double fee; // account transaction fee
+};
