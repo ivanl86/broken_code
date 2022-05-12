240d2da258d4ebe1a9ccf278e380f93bbc751468
Ivan Leung <ivanl8866@gmail.com>
2022-05-11 21:07:28 -0700

    test bank.cpp

--- bank.cpp
+++ bank.cpp
@@ -2,3 +2,18 @@
 // what: Account
 // why: Lab 6a
 // when: May/15/2022
+
+#include <iostream>
+
+#include "savingsAccount.h"
+#include "checkingAccount.h"
+
+int main(int argc, char const *argv[])
+{
+    CheckingAccount ckAcount("customer", 1000, 10);
+
+    std::cout << ckAcount.getBalance();
+
+    return 0;
+}
+
