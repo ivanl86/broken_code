ea0dca537afc7e43143cfcf369c5fae8a30b4d8f
Ivan Leung <ivanl8866@gmail.com>
2022-05-19 00:20:44 -0700

    start testing

---
+++ project_3/building_z.cpp
@@ -0,0 +1,16 @@
+// who: Ivan Leung ileung2
+// what: Building Z
+// why: Project 3
+// when: May/29/2022
+
+#include <iostream>
+
+#include "building.h"
+#include "position.h"
+#include "person.h"
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
