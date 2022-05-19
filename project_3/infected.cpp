3c64dd64b12cace90b334e5432f4a101a79caafc
Ivan Leung <ivanl8866@gmail.com>
2022-05-19 00:15:03 -0700

    add random function

--- project_3/infected.cpp
+++ project_3/infected.cpp
@@ -4,3 +4,6 @@ Infected::Infected(const Position &pos, char type) : Person (pos, type)
 {}
 
 Infected::~Infected() {}
+
+int random(const int &start, const int &end)
+{ return rand() % (end - start + 1) + start; }
