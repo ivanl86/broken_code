ec20f4caf99e4969b8fb8adc03b21f6a672c70e1
Ivan <ivanli8866@gmail.com>
2022-10-24 12:32:53 -0700

    Lab Exam 2

---
+++ csci_150/exam_2/swap.asm
@@ -0,0 +1,38 @@
+; who: <your name and Mt SAC username goes here>
+; what: <the function of this program>
+; why: <the name of the lab>
+; when: < the due date of this lab.>
+
+section     .text
+
+global      _start
+
+_start:
+    mov eax, array
+    mov ebx, array + arraySz - 4
+
+    .while:
+    call swap
+    add eax, 4
+    sub ebx, 4
+    cmp eax, ebx
+    jl  .while
+
+exit:  
+    mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
+    mov     ebx, 0      ; return 0 status on exit - 'No Errors'
+    int     80h
+
+    swap:
+    mov     ecx, [eax]
+    mov     edx, [ebx]
+    xchg    [eax], edx
+    xchg    [ebx], ecx
+
+    ret
+
+section     .bss
+
+section     .data
+    array: dd 1,2,3,4,5,6,7,8,9
+    arraySz: equ $ - array
