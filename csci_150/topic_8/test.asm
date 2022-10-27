b30164b31de41b5de55d4a2170ca3be0fcb0191e
Ivan <ivanli8866@gmail.com>
2022-10-26 17:48:48 -0700

    Lab 8a

--- csci_150/topic_8/test.asm
+++ csci_150/topic_8/test.asm
@@ -1,7 +1,7 @@
-; who: <your name and Mt SAC username goes here>
-; what: <the function of this program>
-; why: <the name of the lab>
-; when: < the due date of this lab.>
+; who: Ivan Leung ileung2
+; what: Swap by ref using local variable and xor operation
+; why: Swap
+; when: 10/30/2022
 
 section     .text
 
