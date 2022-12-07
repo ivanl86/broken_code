9b3a2ec9b341ae0be3a59bab3c5741b6b5390ae8
Ivan <ivanli8866@gmail.com>
2022-12-06 20:40:52 -0800

    Working on stack

---
+++ csci_150/topic_11/stack/main.asm
@@ -0,0 +1,19 @@
+%include "stack.inc"
+%include "../../lib.inc"
+
+section     .text
+
+global      _start
+
+_start:
+
+
+
+exit:  
+    mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
+    mov     ebx, 0      ; return 0 status on exit - 'No Errors'
+    int     80h
+
+section     .bss
+
+section     .data
---
+++ csci_150/topic_11/stack/stack.asm
@@ -0,0 +1,4 @@
+STRUC node
+    .val:   resd 1
+    .next:  resd 1
+ENDSTRUC
