4f2589325234ceba337cca4a041a5caf9003920f
Ivan <ivanli8866@gmail.com>
2022-12-06 21:55:18 -0800

    Stack

--- csci_150/topic_11/stack/main.asm
+++ csci_150/topic_11/stack/main.asm
@@ -6,13 +6,31 @@ section     .text
 global      _start
 
 _start:
+    mov     eax, 0x2d
+    xor     ebx, ebx
+    int     0x80
 
+    mov     edi, eax
 
+    mov     ebx, eax
+    add     ebx, node_size
+    mov     eax, 0x2d
+    int     0x80
 
-exit:  
-    mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
-    mov     ebx, 0      ; return 0 status on exit - 'No Errors'
-    int     80h
+    mov     eax, 8
+    mov     dword [edi + node.val], eax
+
+    mov     dword [edi + node.next], head   ; tmp->next = head
+    mov     dword [head], edi   ; head = tmp
+    mov     dword esi, [head]   ; ESI = head
+
+    push    dword [esi + node.val]
+    call    print_uint
+    call    print_nl
+    add     esp, 4
+
+    push    0
+    call    exit
 
 section     .bss
     head:   resd node_size
