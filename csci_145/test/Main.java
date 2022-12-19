7af8c1a2d0f49fce170275b04e498321fcb310b3
Ivan Leung <ivanl8866@gmail.com>
2022-12-19 14:09:56 -0800

    Java tutorial

---
+++ csci_145/test/Main.java
@@ -0,0 +1,58 @@
+package broken_code.csci_145.test;
+
+// import java.util.Date;
+import java.awt.*;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // byte age = 30;
+        // long viewsCount = 9_123_456_789l;
+        // float price = 10.99f;
+        // char letter = 'A';
+        // boolean isEligible = false;
+        // System.out.println("Hello Java");
+        // System.out.println(age);
+
+        // Date now = new Date();
+        // System.out.println(now);
+
+        // Point point1 = new Point(1, 1);
+        // Point point2 = point1;
+        // point1.x = 2;
+        // System.out.println(point2);
+
+        // String message = "Hello Java" + "!!";
+        
+        // System.out.println(message.toUpperCase());
+        // System.out.println(message);
+
+        // int[] numbers = new int[5];
+        // numbers[0] = 1;
+        // numbers[1] = 2;
+        // int[] numbers = { 2, 3, 5, 1, 4 };
+        // Arrays.sort(numbers);
+        // System.out.println(Arrays.toString(numbers));
+
+        // int[][] numbers = new int [2][3];
+        // int[][] numbers = { { 1, 2, 3 }, { 4, 5, 6 }};
+        // numbers[0][0] = 1;
+        // System.out.println(Arrays.deepToString(numbers));
+
+        // final float pi = 3.14f;
+        // pi = 1;
+
+        // double result = (double)10 / 3;
+        // result += 2;
+        // System.out.println(++result);
+
+        String x = "1";
+        int y = Integer.parseInt(x) + 2;
+        System.out.println(y);
+    }
+}
+
+// C:\Program Files\Java\jdk-19\bin
+// C:\Users\ivanl\AppData\Local\Programs\Microsoft VS Code\bin
