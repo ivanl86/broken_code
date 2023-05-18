8cfe15312df2bba40fac1d9264451c69a99cae3d
Ivan Leung <ivanl8866@gmail.com>
2023-05-17 22:00:29 -0700

    05/17/2023
    Moves to directory sort_n_search
    Adds seqSearch to search.hpp

---
+++ practice/sort_n_search/search.hpp
@@ -0,0 +1,64 @@
+#ifndef SEARCH_HPP
+#define SEARCH_HPP
+
+#include <stdint.h>
+
+template<typename T>
+int64_t seqSearch(const T array[], const uint32_t& size, const T& searchTerm)
+{
+    for(size_t i{0}; i < size; ++i)
+    {
+        if (array[i] == searchTerm)
+            return i;
+    }
+    return -1;
+}
+
+// template<typename T>
+// int64_t seqSearch(const T array[], const uint32_t& size, const T& searchTerm)
+// {
+//     if (size < 1)
+//         return -1;
+//     return (array[size - 1] == searchTerm) ? size - 1 : seqSearch(array, size - 1, searchTerm); 
+// }
+
+template<typename T>
+int64_t binSearch(const T array[], const uint32_t& size, const T& searchTerm)
+{
+    int64_t start{0};
+    int64_t end{size - 1};
+    int64_t mid{};
+
+    while (start <= end)
+    {
+        mid = (start + end) / 2;
+
+        if (searchTerm == array[mid])
+            return mid;
+        else if (searchTerm > array[mid])
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+
+    return -1;
+}
+
+template<typename T>
+int64_t binSearch(const T array[], const uint32_t& size, const T& searchTerm)
+{
+    return binSearch(array, 0, size - 1, searchTerm);
+}
+
+template<typename T>
+int64_t binSearch(const T array[], const int64_t& lowIdx, const int64_t& highIdx, const T& searchTerm)
+{
+    int64_t mid{(lowIdx + highIdx) / 2};
+    if (lowIdx > highIdx)
+        return -1;
+    if (searchTerm == array[mid])
+        return mid;
+    return (searchTerm < array[mid]) ? binSearch(array, lowIdx, mid - 1, searchTerm) : binSearch(array, mid + 1, highIdx, searchTerm);
+}
+
+#endif  /* SEARCH_HPP */
--- practice/sort_n_search/test.cpp
+++ practice/sort_n_search/test.cpp
@@ -1,18 +1,45 @@
 #include <iostream>
 
-#include "bin_search.hpp"
+#include "search.hpp"
+#include "sort.hpp"
+
+// void sizeOfArray(int array[])
+// {
+//     std::cout << "The size of array in func: " << sizeof(array) << "\n";
+// }
 
 int main(int argc, char const *argv[])
 {
-    uint32_t size{10};
-    int term{0};
-    int array[size]{1,2,3,4,5,6,7,8,9};
-    int64_t idx{binSearch(array, size, term)};
-
-    if (idx > -1)
-        std::cout << array[idx] << " at index " << idx << "\n";
+    int idx{};
+    int term{534};
+    int array[] {446,32,534,64,134,69,34,98,174,29};
+    // int array[] {1,2,3,4};
+    // int array[] {4,3,2,1};
+    uint64_t size{sizeof(array) / sizeof(array[0])};
+    for(int element : array)
+    {
+        std::cout << " " << element;
+    }
+    std::cout << "\n";
+    idx = seqSearch(array, size, term);
+    if (idx < 0)
+        std::cout << "Item is not found\n";
     else
-        std::cout << "Item not Found!\n";
-
+        std::cout << array[idx] << " is at index " << idx << "\n";
+    // sort::bubbleSort(array, size);
+    // sort::selectionSort(array, size);
+    // sort::insertionSort(array, size);
+    sort::quickSort(array, size);
+    // swap(array[0], array[1]);
+    for(int element : array)
+    {
+        std::cout << " " << element;
+    }
+    std::cout << "\n";
+    idx = seqSearch(array, size, term);
+    if (idx < 0)
+        std::cout << "Item is not found\n";
+    else
+        std::cout << array[idx] << " is at index " << idx << "\n";
     return 0;
 }
